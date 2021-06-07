45302bc9684bd5c5dde5826285414fba4e9dde79
ishmatov.rus.work <ishmatov.rus@gmail.com>
2021-06-07 10:06:45 +0500

    Основы Django Framework: fix homework02 (json)

---
+++ products/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+import json
+import os
+
+MODULE_DIR = os.path.dirname(__file__)
+
+
+def index(request):
+    context = {
+        'title': 'GeekShop',
+        'header': 'Добро пожаловать на сайт',
+        'username': 'Ишматов Руслан',
+    }
+    return render(request, 'products/index.html', context)
+
+
+def products(request):
+    file_path = os.path.join(MODULE_DIR, 'fixtures/products.json')
+    with open(file_path, "r") as f_products:
+        items = json.load(f_products)
+    context = {
+        'title': 'Каталог товаров GeekShop',
+        'username': 'Ишматов Руслан',
+        'products': items
+    }
+    return render(request, 'products/products.html', context)
