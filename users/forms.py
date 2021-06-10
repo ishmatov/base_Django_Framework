c450e8fbfd4eb7f0f530e00a154343595ee7f332
ishmatov.rus.work <ishmatov.rus@gmail.com>
2021-06-10 15:38:42 +0500

    Основы Django Framework: homework04

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django import forms
 from users.models import User
+from django.core import validators
 
 
 class UserLoginForm(AuthenticationForm):
@@ -26,7 +27,11 @@ class UserRegisterForm(UserCreationForm):
         attrs={
             'class': 'form-control py-4',
             'placeholder': 'Введите имя пользователя'
-        }))
+        }),
+        validators=[validators.RegexValidator(regex='^(?=[a-zA-Z0-9._]{3,20}$)(?!.*[_.]{2})[^_.].*[^_.]$')],
+        error_messages={
+            'invalid': 'Login должен быть длинее 3 символов, И может соделжать латинские буквы, знаки "_" и "." '}
+    )
     email = forms.CharField(widget=forms.EmailInput(
         attrs={
             'class': 'form-control py-4',
@@ -36,12 +41,19 @@ class UserRegisterForm(UserCreationForm):
         attrs={
             'class': 'form-control py-4',
             'placeholder': 'Введите имя'
-        }))
+        }),
+        validators=[validators.RegexValidator(regex='^.{3,}$')],
+        error_messages={'invalid': 'Имя не может быть короче 3 символов'})
     last_name = forms.CharField(widget=forms.TextInput(
         attrs={
             'class': 'form-control py-4',
             'placeholder': 'Введите фамилию'
-        }))
+        }),
+        validators=[validators.RegexValidator(regex='^.{3,}$')],
+        error_messages={'invalid': 'Фамилия не может быть короче 3 символов'}
+    )
+    # image = forms.ImageField()
+
     password1 = forms.CharField(widget=forms.PasswordInput(
         attrs={
             'class': 'form-control py-4',
--- users/models.py
+++ users/models.py
@@ -4,5 +4,3 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     image = models.ImageField(upload_to='users_images', blank=True)
-
-
